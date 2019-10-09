fd3b08bba30004e0e767598b97c750599919a6dd
Anderson Gomes <comprasgomes@hotmail.com>
2019-10-09 14:14:33 -0300

    Create Phpctfxor.php

---
+++ Phpctfxor.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Created by andersongomes001.
+ * Date: 09/10/19
+ */
+
+class Phpctfxor
+{
+
+    private $letras = array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k','l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~');
+    private $a = array();
+    private $b = array();
+    private $c = array();
+    private $palavra = "";
+    /**
+     * Phpfuhex constructor.
+     */
+    public function __construct($palavra)
+    {
+        $this->palavra = $palavra;
+        $this->run();
+    }
+    private function run(){
+        $p = str_split($this->palavra);
+        foreach ($p as $l){
+            $this->getLeter($l);
+        }
+        $this->getPayload();
+    }
+    private function getPayload(){
+        $a = implode($this->a);
+        $b = implode($this->b);
+        $c = implode($this->c);
+        echo "{$this->palavra} = '{$a}'^'{$b}'^'{$c}'\n";
+    }
+    private function getLeter($letra){
+        $valido = false;
+        foreach ($this->letras as $a){
+            foreach ($this->letras as $b){
+                foreach ($this->letras as $c){
+                    if($letra === ( $a^$b^$c)){
+                        if(($a != $b) and ($b != $c) and ($c != $a)){
+                            array_push($this->a,$a);
+                            array_push($this->b,$b);
+                            array_push($this->c,$c);
+                            $valido = true;
+                        }
+                    }
+                }
+                if($valido){
+                    break;
+                }
+            }
+            if($valido){
+                break;
+            }
+        }
+    }
+}
+
